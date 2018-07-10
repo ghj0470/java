ff77974e5b56d714160ca999c79fcf38cda07ebf
test <test@naver.com>
2018-07-10 17:37:31 +0900

    test

--- test/src/test06/Father.java
+++
@@ -1,9 +0,0 @@
-package test06;
-
-public class Father {
-	void work() {
-		System.out.println("아버지가 건설일을 하십니다.");
-	}
-	
-
-}
--- test/src/test06/Sorm.java
+++
@@ -1,8 +0,0 @@
-package test06;
-
-public class Sorm {
-	public void add(int a, int b) {
-	    System.out.println(a + b);
-	    return;
-	}
-}
